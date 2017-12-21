77f97297a3f7e1af02e05b7050192f0987b8c299
Alejandro Lorente <alejandro.lorente@qvantel.com>
2017-12-21 08:15:10 +0100

    configure to use importer submodule

--- ahorrote/settings.py
+++ ahorrote/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'gastos.apps.GastosConfig',
+    'importer.apps.ImporterConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
